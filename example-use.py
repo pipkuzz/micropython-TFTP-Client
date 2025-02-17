f1e3bdf980c0d08262298df32bd0a675b99fa801
Phil Curry <phil@philcurry.co.uk>
2025-02-17 10:49:11 +0000

    Rename testi files
    Rename test files

--- example-use.py
+++ example-use.py
@@ -1,8 +1,8 @@
 # 1. Edit 'your_SSID', 'your_password' and 'your_tftp_server_IP'
 #    to suit your network. Rename this file boot.py and upload
-#    to board toghter with TFTPClient.py and image.svg.
+#    to board toghter with TFTPClient.py and test-image.svg.
 #
-# 2. Upload test-512.txt to your network TFTP server thats
+# 2. Upload test-512-byte.txt to your network TFTP server thats
 #    listening on port 69.
 #
 # 3. reset() your board.
@@ -43,32 +43,32 @@ tftp_client = TFTPClient(TFTP_SERVER_IP)
 # The client returns True on successful completion and False otherwise
 # We can test for that...
 
-# PUT image.svg to TFTP server in default octet mode
-if tftp_client.put_file("image.svg"):
-    print("PUT 'image.svg' ('octet' mode) succsess.")
+# PUT test-image.svg to TFTP server in default octet mode
+if tftp_client.put_file("test-image.svg"):
+    print("PUT 'test-image.svg' ('octet' mode) succsess.")
 
 else:
-    print("PUT 'image.svg' ('octet' mode) failed")
+    print("PUT 'test-image.svg' ('octet' mode) failed")
 
-if tftp_client.put_file("image.svg", 'netascii'):
-    print("PUT 'image.svg' ('netascii' mode) succsess.")
+if tftp_client.put_file("test-image.svg", 'netascii'):
+    print("PUT 'test-image.svg' ('netascii' mode) succsess.")
 
 else:
-    print("PUT 'image.svg' ('netascii' mode) failed")
+    print("PUT 'test-image.svg' ('netascii' mode) failed")
 
 # GET test-512.text from TFTP server in netascii mode
-if tftp_client.get_file("test-512.txt", 'netascii'):
-    print("GET 'test-512.txt' ('netascii' mode) succsess.")
+if tftp_client.get_file("test-512-byte.txt", 'netascii'):
+    print("GET 'test-512-byte.txt' ('netascii' mode) succsess.")
 
 else:
-    print("GET 'test-512.txt' ('netascii' mode) failed")
+    print("GET 'test-512-byte.txt' ('netascii' mode) failed")
 
 # GET test-512.text from TFTP server in octet mode
-if tftp_client.get_file("test-512.txt"):
-    print("GET 'test-512.txt' ('octet' mode) succsess.")
+if tftp_client.get_file("test-512-byte.txt"):
+    print("GET 'test-512-byte.txt' ('octet' mode) succsess.")
 
 else:
-    print("GET 'test-512.txt' ('octet' mode) failed")
+    print("GET 'test-512-byte.txt' ('octet' mode) failed")
 
 # test error handling with non-existant file
 if tftp_client.get_file("no-file-here.txt"):
